3a827fcb05e84bfd4ae64e518e8ff6d5d6630c8f
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2023-04-05 22:38:25 +0800

    Bab 44 Latihan Object

---
+++ 44_object_latihan/note.md
@@ -0,0 +1,9 @@
+# 44 object latihan
+
+Pengelolaan Angkot:
+1. Sopir
+2. Trayek
+3. Kas
+4. Penumpang:
+   - penumpang naik
+   - penumpang turun
---
+++ 44_object_latihan/script.js
@@ -0,0 +1,28 @@
+function Angkot(sopir, trayek, penumpang, kas) {
+    this.sopir = sopir;
+    this.trayek = trayek;
+    this.penumpang = penumpang;
+    this.kas = kas;
+
+    this.penumpangNaik = function(namaPenumpang) {
+        this.penumpang.push(namaPenumpang);
+        return this.penumpang;
+    }
+
+    this.penumpangTurun = function(namaPenumpang, bayar) {
+        if(this.penumpang.length === 0) {
+            console.log('angkot masih kosong');
+            return false;
+        }
+
+        for(var i = 0; i < this.penumpang.length; i++) {
+            if(this.penumpang[i] == namaPenumpang) {
+                this.penumpang[i] = undefined;
+                this.kas += bayar;
+                return this.penumpang;
+            }
+        }
+    }
+}
+
+var angkot1 = new Angkot('Rizqi', ['Sepinggan', 'Manggar'], [], 0);
